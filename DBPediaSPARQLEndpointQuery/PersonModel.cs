3a3225b622cf216be101ddda26e13e1f47a181ab
Egor <zhitnikovegor06032002@gmail.com>
2023-11-19 19:41:37 +0200

    SPARQL Query Class Library

---
+++ DBPediaSPARQLEndpointQuery/PersonModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBPediaSPARQLEndpointQuery
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+
+        public string Child { get; set; }
+        public string Subject { get; set; }
+        public string Spouce { get; set; }
+
+        public string AcademicDiscipline { get; set; }
+        public string Occupation { get; set; }
+
+        public string Founder { get; set; }
+
+        public string Leader { get; set; }
+
+        public string Nominee { get; set; }
+        public string Awards { get; set; }
+
+        public string Office { get; set; }
+        public string KnownFor { get; set; }
+
+        public string Writer { get; set; }
+
+        public PersonModel()
+        {
+            
+        }
+    }
+}
---
+++ DBPediaSPARQLEndpointQuery/Queries.cs
@@ -0,0 +1,36 @@
+﻿using VDS.RDF.Query;
+
+namespace DBPediaSPARQLEndpointQuery
+{
+    public class Queries
+    {
+        public static List<PersonModel> GetAll() 
+        {
+            var result = new List<PersonModel>();
+            SendQuery("SELECT DISTINCT ?name ?birthDate GROUP_CONCAT((?birthPlace); SEPARATOR=\"+\")  AS ?birthPlace ?abstract   GROUP_CONCAT((?subject); SEPARATOR=\"+\")  AS ?subject1  GROUP_CONCAT((?academicDisciplineName); SEPARATOR=\"+\")  AS ?academicDiscipline  GROUP_CONCAT((?occupationName); SEPARATOR=\"+\")  AS ?occupation  GROUP_CONCAT((?awardName ); SEPARATOR=\"+\") AS ?award   GROUP_CONCAT((?officeName); SEPARATOR=\"+\") AS ?office  GROUP_CONCAT((?knownForName); SEPARATOR=\"+\") AS ?knownFor\r\nWHERE\r\n{\r\n?n a dbo:Person.\r\n    ?n dbo:almaMater dbr:Taras_Shevchenko_National_University_of_Kyiv.\r\n    ?n rdfs:label ?name.\r\n\tFILTER langMatches( lang(?name), \"uk\" )\r\n    ?n dbo:abstract ?abstract.\r\n\tFILTER langMatches( lang(?abstract), \"uk\" )\r\n\t?n dbo:birthDate ?birthDate.\r\n\t?n dbo:birthPlace ?birthP.\r\n\t?birthP rdfs:label ?birthPlace.\r\n\tFILTER langMatches( lang(?birthPlace), \"uk\" ) \r\n\tOPTIONAL\r\n\t{\r\n\t?n dbp:subject ?subject.\r\n\t}.\r\n\tOPTIONAL\r\n\t{\r\n\t?n dbo:academicDiscipline ?academicDiscipline.\r\n\t?academicDiscipline rdfs:label ?academicDisciplineName.\r\n\tFILTER langMatches( lang(?academicDisciplineName), \"uk\" )\r\n\t}.\r\n\tOPTIONAL\r\n\t{\r\n\t?n dbp:occupation ?occupation.\r\n\t?occupation rdfs:label ?occupationName.\r\nFILTER langMatches( lang(?occupationName), \"uk\" )\r\n\t}.\r\n\t\r\n\r\n\t\r\nOPTIONAL{\r\n?n dbo:award ?award.\r\n?award rdfs:label ?awardName.\r\nFILTER langMatches( lang(?awardName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:office ?office.\r\n?office rdfs:label ?officeName.\r\nFILTER langMatches( lang(?officeName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:knownFor ?knownFor.\r\n?knownFor rdfs:label ?knownForName.\r\nFILTER langMatches( lang(?knownForName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:leader ?leader.\r\n?leader rdfs:label ?leaderName.\r\nFILTER langMatches( lang(?leaderName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:founder ?founder.\r\n?founder rdfs:label ?founderName.\r\nFILTER langMatches( lang(?founderName), \"uk\" )\r\n}.\r\n\r\n}");
+            
+            return result;
+        }
+
+        public static void GetByName(string Name) 
+        {
+
+        }
+
+        public static SparqlResultSet SendQuery(string query) 
+        {
+            var result = new SparqlResultSet();
+            try
+            {
+                SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
+                result =  endpoint.QueryWithResultSet(query);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.InnerException.Message.ToString());
+            }
+            return result;
+        }
+    }
+}
