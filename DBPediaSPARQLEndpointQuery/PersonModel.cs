9b9b373cfe912c5cf8652330c4aa6f14e8d172dd
Egor <zhitnikovegor06032002@gmail.com>
2023-11-20 17:25:28 +0200

    Added SPARQL SearchByName.

--- DBPediaSPARQLEndpointQuery/PersonModel.cs
+++ DBPediaSPARQLEndpointQuery/PersonModel.cs
@@ -12,25 +12,14 @@ namespace DBPediaSPARQLEndpointQuery
         public string Description { get; set; }
         public string BirthDate { get; set; }
         public string BirthPlace { get; set; }
-
-        public string Child { get; set; }
-        public string Subject { get; set; }
-        public string Spouce { get; set; }
-
         public string AcademicDiscipline { get; set; }
         public string Occupation { get; set; }
-
-        public string Founder { get; set; }
-
-        public string Leader { get; set; }
-
-        public string Nominee { get; set; }
         public string Awards { get; set; }
 
         public string Office { get; set; }
         public string KnownFor { get; set; }
+        public string Img { get; set; }
 
-        public string Writer { get; set; }
 
         public PersonModel()
         {
--- DBPediaSPARQLEndpointQuery/Queries.cs
+++ DBPediaSPARQLEndpointQuery/Queries.cs
@@ -1,4 +1,6 @@
-﻿using VDS.RDF.Query;
+﻿using AngleSharp.Common;
+using System;
+using VDS.RDF.Query;
 
 namespace DBPediaSPARQLEndpointQuery
 {
@@ -7,14 +9,32 @@ namespace DBPediaSPARQLEndpointQuery
         public static List<PersonModel> GetAll() 
         {
             var result = new List<PersonModel>();
-            SendQuery("SELECT DISTINCT ?name ?birthDate GROUP_CONCAT((?birthPlace); SEPARATOR=\"+\")  AS ?birthPlace ?abstract   GROUP_CONCAT((?subject); SEPARATOR=\"+\")  AS ?subject1  GROUP_CONCAT((?academicDisciplineName); SEPARATOR=\"+\")  AS ?academicDiscipline  GROUP_CONCAT((?occupationName); SEPARATOR=\"+\")  AS ?occupation  GROUP_CONCAT((?awardName ); SEPARATOR=\"+\") AS ?award   GROUP_CONCAT((?officeName); SEPARATOR=\"+\") AS ?office  GROUP_CONCAT((?knownForName); SEPARATOR=\"+\") AS ?knownFor\r\nWHERE\r\n{\r\n?n a dbo:Person.\r\n    ?n dbo:almaMater dbr:Taras_Shevchenko_National_University_of_Kyiv.\r\n    ?n rdfs:label ?name.\r\n\tFILTER langMatches( lang(?name), \"uk\" )\r\n    ?n dbo:abstract ?abstract.\r\n\tFILTER langMatches( lang(?abstract), \"uk\" )\r\n\t?n dbo:birthDate ?birthDate.\r\n\t?n dbo:birthPlace ?birthP.\r\n\t?birthP rdfs:label ?birthPlace.\r\n\tFILTER langMatches( lang(?birthPlace), \"uk\" ) \r\n\tOPTIONAL\r\n\t{\r\n\t?n dbp:subject ?subject.\r\n\t}.\r\n\tOPTIONAL\r\n\t{\r\n\t?n dbo:academicDiscipline ?academicDiscipline.\r\n\t?academicDiscipline rdfs:label ?academicDisciplineName.\r\n\tFILTER langMatches( lang(?academicDisciplineName), \"uk\" )\r\n\t}.\r\n\tOPTIONAL\r\n\t{\r\n\t?n dbp:occupation ?occupation.\r\n\t?occupation rdfs:label ?occupationName.\r\nFILTER langMatches( lang(?occupationName), \"uk\" )\r\n\t}.\r\n\t\r\n\r\n\t\r\nOPTIONAL{\r\n?n dbo:award ?award.\r\n?award rdfs:label ?awardName.\r\nFILTER langMatches( lang(?awardName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:office ?office.\r\n?office rdfs:label ?officeName.\r\nFILTER langMatches( lang(?officeName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:knownFor ?knownFor.\r\n?knownFor rdfs:label ?knownForName.\r\nFILTER langMatches( lang(?knownForName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:leader ?leader.\r\n?leader rdfs:label ?leaderName.\r\nFILTER langMatches( lang(?leaderName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:founder ?founder.\r\n?founder rdfs:label ?founderName.\r\nFILTER langMatches( lang(?founderName), \"uk\" )\r\n}.\r\n\r\n}");
-            
+            var dataset=SendQuery(
+                "SELECT DISTINCT ?name ?img ?birthDate GROUP_CONCAT((?birthPlace); SEPARATOR=\"+\")  AS ?birthPlace     GROUP_CONCAT((?occupationName); SEPARATOR=\"+\")  AS ?occupation ?abstract GROUP_CONCAT((?awardName ); SEPARATOR=\"+\") AS ?award   GROUP_CONCAT((?officeName); SEPARATOR=\"+\") AS ?office  GROUP_CONCAT((?knownForName); SEPARATOR=\"+\") AS ?knownFor\r\nWHERE\r\n{\r\n?n a dbo:Person.\r\n?n dbo:thumbnail ?img.\r\n    ?n dbo:almaMater dbr:Taras_Shevchenko_National_University_of_Kyiv.\r\n    ?n rdfs:label ?name.\r\n\tFILTER langMatches( lang(?name), \"uk\" )\r\n    ?n dbo:abstract ?abstract.\r\n\tFILTER langMatches( lang(?abstract), \"uk\" )\r\n\t?n dbo:birthDate ?birthDate.\r\n\t?n dbo:birthPlace ?birthP.\r\n\t?birthP rdfs:label ?birthPlace.\r\n\tFILTER langMatches( lang(?birthPlace), \"uk\" ) \r\n\r\n OPTIONAL\r\n  {\r\n  ?n dbo:academicDiscipline ?academicDiscipline.\r\n  ?academicDiscipline rdfs:label ?academicDisciplineName.\r\n  FILTER langMatches( lang(?academicDisciplineName), \"uk\" )\r\n  }.\r\n   \r\nOPTIONAL{\r\n?n dbo:award ?award.\r\n?award rdfs:label ?awardName.\r\nFILTER langMatches( lang(?awardName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:office ?office.\r\n?office rdfs:label ?officeName.\r\nFILTER langMatches( lang(?officeName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:knownFor ?knownFor.\r\n?knownFor rdfs:label ?knownForName.\r\nFILTER langMatches( lang(?knownForName), \"uk\" )\r\n}.\r\n}"
+                );
+            foreach (var item in dataset)
+            {
+                result.Add(FromRDFToPersonModel(item));
+            }
             return result;
         }
 
-        public static void GetByName(string Name) 
+        public static SearchStatusEnum SearchByName(string name,out List<PersonModel> model) 
         {
-
+            model = new List<PersonModel>();
+            var rset = SendQuery(
+                "SELECT DISTINCT ?name ?birthDate GROUP_CONCAT((?birthPlace); SEPARATOR=\"+\")  AS ?birthPlace   GROUP_CONCAT((?occupationName); SEPARATOR=\"+\")  AS ?occupation ?abstract GROUP_CONCAT((?awardName ); SEPARATOR=\"+\") AS ?award   GROUP_CONCAT((?officeName); SEPARATOR=\"+\") AS ?office  GROUP_CONCAT((?knownForName); SEPARATOR=\"+\") AS ?knownFor\r\nWHERE\r\n{\r\n?n a dbo:Person.\r\n    ?n dbo:almaMater dbr:Taras_Shevchenko_National_University_of_Kyiv.\r\n    ?n rdfs:label  ?name.\r\n  FILTER(REGEX(?name, \"" + name + "\",\"i\"))\r\nFILTER langMatches( lang(?name), \"uk\" )\r\n    ?n dbo:abstract ?abstract.\r\n\tFILTER langMatches( lang(?abstract), \"uk\" )\r\n\t?n dbo:birthDate ?birthDate.\r\n\t?n dbo:birthPlace ?birthP.\r\n\t?birthP rdfs:label ?birthPlace.\r\n\tFILTER langMatches( lang(?birthPlace), \"uk\" ) \r\nOPTIONAL\r\n{\r\n?n dbo:award ?award.\r\n?award rdfs:label ?awardName.\r\nFILTER langMatches( lang(?awardName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:office ?office.\r\n?office rdfs:label ?officeName.\r\nFILTER langMatches( lang(?officeName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:knownFor ?knownFor.\r\n?knownFor rdfs:label ?knownForName.\r\nFILTER langMatches( lang(?knownForName), \"uk\" )\r\n}.\r\nOPTIONAL\r\n{\r\n?n dbp:occupation ?occupation.\r\n?occupation rdfs:label ?occupationName.\r\nFILTER langMatches( lang(?occupationName), \"uk\" )\r\n}.\r\n}"
+                );
+            if (rset.Count > 0)
+            {
+                foreach (var item in rset) 
+                {
+                    model.Add(FromRDFToPersonModel(item));
+                }               
+                return SearchStatusEnum.Success;
+            }
+            return SearchStatusEnum.No_matches;
+            
         }
 
         public static SparqlResultSet SendQuery(string query) 
@@ -31,6 +51,47 @@ namespace DBPediaSPARQLEndpointQuery
                 Console.WriteLine(ex.InnerException.Message.ToString());
             }
             return result;
+        }
+
+        static List<string> GetUniqueStrings(string info)
+        {
+            var input = info.Split('+').ToList();
+            List<string> uniqueList = input.Distinct().ToList();
+            return uniqueList;
+        }
+
+        static string ConcatStrings(List<string> list)
+        {
+            string info = string.Empty;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                info += list[i] + ", ";
+            }
+            return info += list[list.Count - 1];
+        }
+
+        static PersonModel FromRDFToPersonModel(ISparqlResult item) 
+        {
+            var person = new List<string>();
+            for (int i = 0; i < item.Count; i++)
+            {
+                var a = item[i].ToDictionary();
+                string f = string.Empty;
+                a.TryGetValue("Value", out f);
+                if (f == null) a.TryGetValue("Uri", out f);
+                person.Add(f);
+            }
+            var model = new PersonModel();
+            model.Name = person[0];
+            model.Img = person[1];
+            model.BirthDate = person[2];
+            model.BirthPlace = ConcatStrings(GetUniqueStrings(person[3]));
+            model.Occupation = ConcatStrings(GetUniqueStrings(person[4]));
+            model.Description = person[5];
+            model.Awards = ConcatStrings(GetUniqueStrings(person[6]));
+            model.Office = ConcatStrings(GetUniqueStrings(person[7]));
+            model.KnownFor = ConcatStrings(GetUniqueStrings(person[8]));
+            return model;
         }
     }
 }
